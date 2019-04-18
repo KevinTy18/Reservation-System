24374dcbf072bdf481eecb61dd0a1d55f4afa68d
MichaelBaldevia <baldeviamichael1@gmail.com>
2019-04-18 16:16:55 +0800

    Added the Calendar Cookie Crumbs Function

--- includes/CheckCalendarNavigation.php
+++ includes/CheckCalendarNavigation.php
@@ -1,9 +1,15 @@
 <div class="buttons">
 <form action="checkbookings.php" method="POST">
     <!--<input type="submit" value="Check Calendar" /> -->
-    <button class="smallbuttonnav" type="submit" name="AllDepartments"
-style="float:left"><span><i class="fa
+   <?php if ($_SESSION['Department'] == " ")  { ?>
+    <button class="currentsmallbuttonnav" type="submit" name="AllDepartments" style="float:left;background-color:black"><span><i class="fa fa-calendar" style="font-size:15px;color:red"></i> All Departments</span></button>
+ <?php  }
+ else { ?>
+
+ <button class="smallbuttonnav" type="submit" name="AllDepartments"
+style="float:left;background-color:black"><span><i class="fa
 fa-calendar" style="font-size:15px;color:red"></i> All Departments</span></button>
+<?php }?>
 </form>
 </div>
 <?php 
@@ -20,10 +26,21 @@ $result = $conn->query($sql);
 if ($result->num_rows > 0) {
     // output data of each row
     while($row = $result->fetch_assoc()) {
+    	/*$CurrentDepartment = */
+    
     echo  '<div class="buttons">';
     echo  '<form action="checkbookings.php" method="POST">';
-    echo  '<button class="smallbuttonnav" type="submit" name='. $row['Department'] . ' style="float:left"><span><i class="fa fa-calendar"
+	    	/*echo '<pre>';
+	        die(var_dump($row['Id']));
+	        echo '</pre>';*/
+    if ("AND Room_Department = " . $row['Id']	 == $_SESSION['Department']) {
+    	 echo  '<button class="currentsmallbuttonnav" type="submit" name='. $row['Department'] . ' style="float:left; background-color:red"><span><i class="fa fa-calendar"
       style="font-size:15px;color:red"></i>'. $row['Department']  .'</span></button>';
+    }
+   else {
+   	 echo  '<button class="smallbuttonnav" type="submit" name='. $row['Department'] . ' style="float:left;"><span><i class="fa fa-calendar"
+      style="font-size:15px;color:red"></i>'. $row['Department']  .'</span></button>';
+   }
     echo  '</form>';
     echo  '</div>';
     }
